8628653d6c7ce6fc8e563c3a2999ac294ca3edef
Akshata72 <sableakshata880@gmail.com>
2022-05-04 06:12:49 +0530

    Added:UC1-CreateDataTable

---
+++ AddressBookLinq/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook_Linq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address book linq problem!");
+            AddressBook addressBook = new AddressBook();
+
+        }
+    }
+}
